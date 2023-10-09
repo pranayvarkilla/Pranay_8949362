250f0ba2e19579a4601fa4116c558ae519d3ceca
Pranayvarkilla <pranayvarkilla@gmail.com>
2023-10-09 16:36:11 -0400

    uploaded

--- Assignment 3/Pranay_assignment3.py
+++ Assignment 3/Pranay_assignment3.py
@@ -1,15 +1,11 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
 
-
-
 driver.get("https://www.youtube.com")
 time.sleep(3)
-
 
 search_bar = driver.find_element("name", "search_query")
 search_bar.send_keys("music videos")
@@ -25,14 +21,11 @@ first_video.click()
 
 time.sleep(10)
 
-
 pause_button = driver.find_element("xpath", "//button[@class='ytp-play-button ytp-button']")
 pause_button.click()
 
-
 volume_button = driver.find_element("xpath", "//button[@class='ytp-mute-button ytp-button']")
 volume_button.click()
-
 
 youtube_logo = driver.find_element("xpath", "//a[@id='logo-icon-container']")
 youtube_logo.click()
